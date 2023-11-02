78a78541007f876d000bf029dee41019257a3a52
ObjectsCountries <oandcofficial@gmail.com>
2023-11-02 15:31:18 -0700

    Add main page 21, attempt to add ruleseed to MP's manual

--- Assets/_Main Page/_mpAnims.cs
+++ Assets/_Main Page/_mpAnims.cs
@@ -57,7 +57,8 @@ public class _mpAnims:MonoBehaviour{
             setup(0      , .0235f,.16f  ,.075f ,false, .85f                      ),
             setup(0      , .0235f,.1575f,.035f ,false, .9f                       ),
             setup(0      , .0235f,.1575f,.1f   ,false                            ),
-            setup(-.0233f, .01f  ,.2063f,.075f ,false, .85f,xOffset:.1f          )
+            setup(-.0233f, .01f  ,.2063f,.075f ,false, .85f,xOffset:.1f          ),
+            setup( .01f  , .04f  ,.1f   ,.1f   ,false                            )
         };
         b.SetActive(false);
         animations = new IEnumerator[]{
@@ -80,7 +81,8 @@ public class _mpAnims:MonoBehaviour{
             A(TXTRs.FR17,true),
             A(TXTRs.FR18,true),
             A(TXTRs.FR19,false),
-            A(TXTRs.FR20,true)
+            A(TXTRs.FR20,true),
+            A(TXTRs.FR21,true)
         };
         animNum = Random.Range(0, animations.Length);
         while (takenAnims.Contains(animNum)) animNum = Random.Range(0, animations.Length);
--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 
 * Strong Mad Talker: **Finished**
   * Credit boiaaang for color design
+  * Add ruleseed support
 * Main Page: **In Progress**
-  * 18/27 main pages implemented
+  * 21/27 main pages implemented
 * Sbemail Songs: **Not Started**
-  * Decide between non-boss and boss concepts (musing [here](https://discord.com/channels/160061833166716928/1138212224050147388))
 * Bronco Trolleys (Needy): **Finished**
   * Might get rid of parameter to change number required
 * Sid Hoffrenchman (Needy): **Finished**
